a3acf12c7921833fa70104fa78f621d0abda2e3a
Pedro Kimura <122410185+PedroKimura10@users.noreply.github.com>
2026-03-16 13:16:59 -0300

    Menu principal
    Fizemos a tela de menu e a de opções

---
+++ src/jogo/Assets/strings/MenuPrincipalMeneger.cs
@@ -0,0 +1,37 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MenuPrincipalMeneger : MonoBehaviour
+{
+    [SerializeField] private string nomeDeLevelDeJogo;
+    [SerializeField] private GameObject CanvaSom, CanvaMenu;
+
+    public void Jogar()
+    {
+        SceneManager.LoadScene("SampleScene");
+    }
+
+    public void AbrirOpcoes()
+    {
+        CanvaMenu.SetActive(false);
+        CanvaSom.SetActive(true);
+    }
+
+    public void FecharOpcoes()
+    {
+        CanvaMenu.SetActive(true);
+        CanvaSom.SetActive(false);
+
+    }
+
+    public void SairJogo()
+    { 
+
+    }
+}
+
+
+
+
+
+
