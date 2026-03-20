ce720f2fda84bcfef42c2d9b5840fe32b1564c21
Pedro Kimura <122410185+PedroKimura10@users.noreply.github.com>
2026-03-20 11:46:36 -0300

    Labririnto
    Fizemos o labirinto

--- src/jogo/Assets/strings/personagem.cs
+++ src/jogo/Assets/strings/personagem.cs
@@ -2,7 +2,7 @@ using UnityEngine;
 
 public class personagem : MonoBehaviour
 {
-    public float speed = 35f;
+    public float speed = 5f;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
@@ -12,23 +12,23 @@ public class personagem : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        transform.Rotate(Vector3.left * Time.deltaTime * 80f);
+        
         
         if (Input.GetKey(KeyCode.UpArrow))
         {
-            transform.Translate(Vector3.forward * Time.deltaTime * 60f);
+            transform.Translate(Vector3.forward * Time.deltaTime * speed);
         }
         if (Input.GetKey(KeyCode.DownArrow))
         {
-            transform.Translate(Vector3.back * Time.deltaTime * 60f);
+            transform.Translate(Vector3.back * Time.deltaTime * speed);
         }
         if (Input.GetKey(KeyCode.LeftArrow))
         {
-            transform.Translate(Vector3.left * Time.deltaTime * 60f);
+            transform.Translate(Vector3.left * Time.deltaTime * speed);
         }
         if (Input.GetKey(KeyCode.RightArrow))
         {
-            transform.Translate(Vector3.right * Time.deltaTime * 60f);
+            transform.Translate(Vector3.right * Time.deltaTime * speed);
         }
     }
 }
