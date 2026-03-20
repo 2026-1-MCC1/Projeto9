f664b8a28efc93d0642fbd2cb35876ea49928680
Gabriel Ribeiro <delgm2020@gmail.com>
2026-03-20 11:02:50 -0300

    ponto inicial
    tentativa

--- src/jogo/Assets/strings/personagem.cs
+++ src/jogo/Assets/strings/personagem.cs
@@ -2,33 +2,35 @@ using UnityEngine;
 
 public class personagem : MonoBehaviour
 {
-    public float speed = 35f;
+    public float speed = 5f;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
+
+    public Transform spawnPoint; //ponto inicial mapa
+    public GameObject player;
     void Start()
     {
         Debug.Log("personagem inicio.");
+        Instantiate(player, spawnPoint.position, spawnPoint.rotation); //ponto inicial
     }
 
     // Update is called once per frame
     void Update()
-    {
-        transform.Rotate(Vector3.left * Time.deltaTime * 80f);
-        
-        if (Input.GetKey(KeyCode.UpArrow))
+    { 
+        if (Input.GetKey(KeyCode.UpArrow)) //movimentações
         {
-            transform.Translate(Vector3.forward * Time.deltaTime * 60f);
+            transform.Translate(Vector3.forward * Time.deltaTime * speed);
         }
         if (Input.GetKey(KeyCode.DownArrow))
         {
-            transform.Translate(Vector3.back * Time.deltaTime * 60f);
+            transform.Translate(Vector3.back * Time.deltaTime * speed);
         }
         if (Input.GetKey(KeyCode.LeftArrow))
         {
-            transform.Translate(Vector3.left * Time.deltaTime * 60f);
+            transform.Translate(Vector3.left * Time.deltaTime * speed);
         }
         if (Input.GetKey(KeyCode.RightArrow))
         {
-            transform.Translate(Vector3.right * Time.deltaTime * 60f);
+            transform.Translate(Vector3.right * Time.deltaTime * speed);
         }
     }
 }
