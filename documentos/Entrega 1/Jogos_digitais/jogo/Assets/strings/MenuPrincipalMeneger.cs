02d4ffa5b458c1f9e728b41bd9e6c139f1a1b2dd
joaocroti <jgabrielramalhocroti@gmail.com>
2026-04-10 08:24:00 -0300

    Adicionado o jogo
    Foi adicionado o jogo a entrega 1/ jogos digitais

---
+++ documentos/Entrega 1/Jogos_digitais/jogo/Assets/strings/MenuPrincipalMeneger.cs
@@ -0,0 +1,46 @@
+using UnityEditor;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MenuPrincipalMeneger : MonoBehaviour
+{
+    [SerializeField] private string nomeDeLevelDeJogo;
+    [SerializeField] private GameObject CanvaSom, CanvaMenu;
+
+    //---Carrega a cena do jogo---
+    public void Jogar()
+    {
+        SceneManager.LoadScene("labirinto");
+    }
+
+    //---Abre o menu de opções---
+    public void AbrirOpcoes() 
+    {
+        CanvaMenu.SetActive(false);
+        CanvaSom.SetActive(true);
+        
+    }
+
+    //---Fecha o menu de opções---
+    public void FecharOpcoes()
+    {
+        CanvaMenu.SetActive(true);
+        CanvaSom.SetActive(false);
+       
+
+    }
+
+    //---Fecha o jogo---
+    public void SairJogo() 
+    {
+        Debug.Log("Fechando o game");
+        Application.Quit();
+
+    }
+}
+
+
+
+
+
+
